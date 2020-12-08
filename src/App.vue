a595a3482d6dc2d488dadf045c45178760fe95e8
yablko <yablko@brm.sk>
2020-12-08 21:48:48 +0100

    01-VITE-KOMPONENTY

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld message="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-	name: "App",
-	components: {
-		HelloWorld,
-	},
-};
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+}
 </script>
 
 <style scoped>
 img {
-	width: 30%;
-	padding: 1.5em;
-	box-shadow: 0 0 10px black;
+  width: 30%;
+  padding: 1.5em;
+  box-shadow: 0 0 10px black;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,32 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ message }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
+  props: ['message'],
   data() {
     return {
-      count: 0
+      count: 0,
     }
-  }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+$bg-color: white;
+
+p {
+  code {
+    color: red;
+    background: $bg-color;
+    padding: 7px;
+    border-radius: 3px;
+  }
+}
+</style>
